26ce175ca95d125f9e62e87362937b22cf86aea1
tobuw <tobuwidjaja@gmail.com>
2022-09-07 21:50:29 -0400

    notes

---
+++ features/TestPracticeFeature/Test3.feature
@@ -0,0 +1,8 @@
+Feature: Test3
+    @test3
+    Scenario: Darksky2
+        Given I am on darksky landing page
+        When I scroll to today timeline
+        And I click on plus button
+        Then I verify minTemp and todayMinTemp are the same
+        Then I verify maxTemp and todayMaxTemp are the same
---
+++ step-definitions/TestPracticeSD/TestSD3.js
@@ -0,0 +1,19 @@
+const { When } = require("@wdio/cucumber-framework");
+const { expect } = require("chai");
+const testObj3 = require("../../Pages/TestPracticePages/TestPage3");
+const test = new testObj3();
+
+When(/^I scroll to today timeline$/, async function(){
+    await test.scrollToTodayTimeline()
+    await browser.pause(2000)
+})
+When(/^I click on plus button$/, async function(){
+    await test.clickOnPlusButton()
+    await browser.pause(2000)
+})
+When(/^I verify minTemp and todayMinTemp are the same$/, async function(){
+    expect(await test.getTodayMinTemp(), "not same").to.be.equal(await test.getTodayLowTempSwap())
+})
+When(/^I verify maxTemp and todayMaxTemp are the same$/, async function(){
+    expect(await test.getTodayMaxTemp(), "not same").to.be.equal(await test.getTodayHighTempSwap())
+})
